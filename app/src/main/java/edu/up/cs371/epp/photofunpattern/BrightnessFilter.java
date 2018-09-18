80c8acff7e86c60f0107af88155967d7a9531a43
langa21 <langa21@up.edu>
2018-09-18 10:58:45 -0700

    West edge filter

--- app/src/main/java/edu/up/cs371/epp/photofunpattern/BrightnessFilter.java
+++ app/src/main/java/edu/up/cs371/epp/photofunpattern/BrightnessFilter.java
@@ -1,5 +1,6 @@
 package edu.up.cs371.epp.photofunpattern;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
 
 /**
@@ -23,12 +24,12 @@ public class BrightnessFilter extends PhotoFilter {
     * @param inPixel is a 32 bit pixel that contains RGB color values
     * @return a new Pixel in which each of the RGB components has been increased
     */
-    public int transformPixel(int inPixel) {
-        int red = constrain(Color.red(inPixel) + ADJUSTMENT);
-        int green = constrain(Color.green(inPixel) + ADJUSTMENT);
-        int blue = constrain(Color.blue(inPixel) + ADJUSTMENT);
-        int outPixel = Color.argb(Color.alpha(inPixel), red, green, blue);
-        return outPixel;
+    public int transformPixel(int [] arr) {
+
+
+        int s = (arr[0] + arr[1] + arr[2] * -1 + arr[3] + arr[4] * -2 + arr[5] * -1 + arr[6] + arr[7] + arr[8] * -1) / 9;
+
+        return s;
     }
 
 }
